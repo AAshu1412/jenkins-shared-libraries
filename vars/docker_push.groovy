dff204b2e1dccb237623456b8f81baebdb9a53fa
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2026-02-24 04:37:15 +0530

    done

---
+++ vars/docker_push.groovy
@@ -0,0 +1,12 @@
+def call (String credId, String imageName){
+     withCredentials([usernamePassword(
+                            credentialsId:"${credId}",
+                            passwordVariable:"dockerhubPass",
+                            usernameVariable:"dockerhubUser")]) {
+                                
+                                sh "docker login -u ${env.dockerhubUser} -p ${env.dockerhubPass}"    
+                                sh "docker image tag ${imageName} ${env.dockerhubUser}/${imageName}"    
+                                sh "docker push ${env.dockerhubUser}/${imageName}:latest"
+                                
+                        }
+}
